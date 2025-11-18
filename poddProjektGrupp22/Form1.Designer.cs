a22527830bbff19405cc0b2803251519760e49e5
P-P-R <paulus.razai.petrus@gmail.com>
2025-11-18 15:31:32 +0100

    Fixa design
    .

--- poddProjektGrupp22/Form1.Designer.cs
+++ poddProjektGrupp22/Form1.Designer.cs
@@ -28,45 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            label1 = new Label();
+            textBoxURL = new TextBox();
+            buttonNyhetskalla = new Button();
+            listBoxInformation = new ListBox();
+            listBoxLankar = new ListBox();
+            buttonSpara = new Button();
+            buttonVisning = new Button();
             SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            button1.Location = new Point(398, 132);
-            button1.Name = "button1";
-            button1.Size = new Size(112, 34);
-            button1.TabIndex = 0;
-            button1.Text = "Kyri";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(162, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Ange källans url (RSS-adress):";
             // 
-            // button2
+            // textBoxURL
             // 
-            button2.Location = new Point(581, 137);
-            button2.Name = "button2";
-            button2.Size = new Size(112, 34);
-            button2.TabIndex = 1;
-            button2.Text = "Phablo";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            textBoxURL.Location = new Point(12, 27);
+            textBoxURL.Name = "textBoxURL";
+            textBoxURL.Size = new Size(261, 23);
+            textBoxURL.TabIndex = 3;
+            // 
+            // buttonNyhetskalla
+            // 
+            buttonNyhetskalla.Location = new Point(12, 56);
+            buttonNyhetskalla.Name = "buttonNyhetskalla";
+            buttonNyhetskalla.Size = new Size(162, 23);
+            buttonNyhetskalla.TabIndex = 4;
+            buttonNyhetskalla.Text = "Hämta och visa nyhetskälla";
+            buttonNyhetskalla.UseVisualStyleBackColor = true;
+            // 
+            // listBoxInformation
+            // 
+            listBoxInformation.FormattingEnabled = true;
+            listBoxInformation.Location = new Point(12, 104);
+            listBoxInformation.Name = "listBoxInformation";
+            listBoxInformation.Size = new Size(303, 289);
+            listBoxInformation.TabIndex = 5;
+            // 
+            // listBoxLankar
+            // 
+            listBoxLankar.FormattingEnabled = true;
+            listBoxLankar.Location = new Point(349, 104);
+            listBoxLankar.Name = "listBoxLankar";
+            listBoxLankar.Size = new Size(313, 289);
+            listBoxLankar.TabIndex = 6;
+            // 
+            // buttonSpara
+            // 
+            buttonSpara.Location = new Point(12, 406);
+            buttonSpara.Name = "buttonSpara";
+            buttonSpara.Size = new Size(162, 23);
+            buttonSpara.TabIndex = 7;
+            buttonSpara.Text = "Spara i nyhetsflödet";
+            buttonSpara.UseVisualStyleBackColor = true;
+            // 
+            // buttonVisning
+            // 
+            buttonVisning.Location = new Point(224, 406);
+            buttonVisning.Name = "buttonVisning";
+            buttonVisning.Size = new Size(151, 23);
+            buttonVisning.TabIndex = 8;
+            buttonVisning.Text = "Ange visningsnamn";
+            buttonVisning.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(711, 441);
+            Controls.Add(buttonVisning);
+            Controls.Add(buttonSpara);
+            Controls.Add(listBoxLankar);
+            Controls.Add(listBoxInformation);
+            Controls.Add(buttonNyhetskalla);
+            Controls.Add(textBoxURL);
+            Controls.Add(label1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private Button button1;
-        private Button button2;
+        private Label label1;
+        private TextBox textBoxURL;
+        private Button buttonNyhetskalla;
+        private ListBox listBoxInformation;
+        private ListBox listBoxLankar;
+        private Button buttonSpara;
+        private Button buttonVisning;
     }
 }
