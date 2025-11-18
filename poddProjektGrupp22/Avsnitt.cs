ca25bea81edc94227829cd12d78081cbb058f037
P-P-R <paulus.razai.petrus@gmail.com>
2025-11-18 16:37:26 +0100

    Alla klasser
    .

---
+++ poddProjektGrupp22/Avsnitt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poddProjektGrupp22
+{
+    internal class Avsnitt
+    {
+    }
+}
---
+++ poddProjektGrupp22/RegisterForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace poddProjektGrupp22
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
